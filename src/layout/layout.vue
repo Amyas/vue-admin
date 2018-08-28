9181ab6cfc3b4c015f0a2c2697787235e91830ae
王建鹏 <amyas.wang@qingclass.com>
2018-08-28 17:38:31 +0800

    新增 用户模块 route

---
+++ src/layout/layout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="layout">
+    <div class="l">左侧</div>
+    <div class="r">
+      <div>面包屑</div>
+      <div class="content">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Home from '@/views/Home/index'
+const Layout = () => import('@/layout/layout')
 
-const User = () => import(/* webpackChunkName: "about" */ '@/views/User')
+const UserList = () => import('@/views/User/list')
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
+    {// Index首页模块
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'index',
+      redirect: 'user'
     },
-    {
+    {// User用户模块
       path: '/user',
       name: 'user',
-      component: User
+      meta: {
+        routeName: '用户管理',
+        iconClass: ''
+      },
+      component: Layout,
+      redirect: '/user/list',
+      children: [
+        {
+          path: 'list',
+          name: 'userlist',
+          meta: {
+            routeName: '用户列表'
+          },
+          component: UserList
+        }
+      ]
     }
   ]
 })
