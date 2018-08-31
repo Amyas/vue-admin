4f36d76e81e389c9a6ec703b4a49e3cc76e22f90
王建鹏 <amyas.wang@qingclass.com>
2018-08-31 11:53:38 +0800

    优化 main.js import 写法

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import 'normalize.css'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'nprogress/nprogress.css'
 
-import utils from '@/utils'
+import utils from './utils'
 import directive from './utils/directive'
 
 sync(store, router)
